22584802cc403710cbfc52a563b3337eaa70b8cf
yiguopan <yiguopan@126.com>
2015-01-11 21:06:36 +0800

    Update parse_ini_file.c

--- parse_ini_file.c
+++ parse_ini_file.c
@@ -1,27 +1,30 @@
 //
-//  parse_ini_file.c
-//  paser_ini_file
+//  main.c
+//  ini
 //
-//  Created by 易国磐 on 15-1-3.
+//  Created by 易国磐 on 15-1-9.
 //  Copyright (c) 2015年 易国磐. All rights reserved.
 //
 
-#include "parse_ini_file.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-/***
- * 根据分割符，切分字符串
- *
- * @param char*     str
- * @param size_t    strlen
- * @param char*     delm
- * @param size_t    delmlen
- *
- * @return char **
- */
+#include "ini.h"
+
+#define MALLOC_INI_PARAMS   (ini_params *)malloc(sizeof(ini_params))
+#define MALLOC_INI          (ini *)malloc(sizeof(ini))
+
+#define Z_ERROR              -1
+#define Z_SUCCESS             0
+#define RETURN_NULL          return NULL
+
 char **str_split(char *src, size_t strlen, const char *delm, size_t delmlen)
 {
     // 需要修改字符串，提前复制数据源
     char *str = (char *)malloc(sizeof(char) * strlen + 1);
+    if (!str) RETURN_NULL;
+    
     memcpy(str, src, strlen);
     int i = 0;
     int c = 0;
@@ -45,117 +48,223 @@ char **str_split(char *src, size_t strlen, const char *delm, size_t delmlen)
         }
     }
     *(str + i) = '\0';
+    free(str);
     return res;
 }
 
 
-ini *make_ini(const char *key, const char *value)
+int is_empty(const char *str)
 {
-    ini *node = (ini *)malloc(sizeof(ini));
-    strcpy(node->key, key);
-    strcpy(node->value, value);
+    size_t len = strlen(str);
+    int i;
+    for (i = 0; i < len; i++) {
+        if (!*(str+i)) {
+            return Z_ERROR;
+        }
+    }
     
-    node->next = NULL;
-    return node;
+    return Z_SUCCESS;
 }
 
-void ini_set(ini **node, ini *i)
+int strpos(const char *str, const char *find)
 {
-    i->next = *node;
-    *node = i;
+    size_t findlen = strlen(find);
+    size_t str_len = strlen(str);
+ 
+    int i = 0;
+    do {
+        if (memcmp(str + i, find, findlen) == 0) {
+            return i;
+        }
+    } while (i++ < str_len);
+    
+    return Z_ERROR;
+}
+
+ini_params *create_ini_params(const char *key, const char *value)
+{
+    if (is_empty(key) || is_empty(value)) {
+        RETURN_NULL;
+    }
+    
+    ini_params *init_param_t = MALLOC_INI_PARAMS;
+    if (!init_param_t) {
+        RETURN_NULL;
+    }
+    
+    memset(init_param_t,        0, sizeof(ini_params));
+    memset(init_param_t->key,   0, sizeof(init_param_t->key));
+    memset(init_param_t->value, 0, sizeof(init_param_t->value));
+    
+    memcpy(init_param_t->key,   key, strlen(key));
+    memcpy(init_param_t->value, value, strlen(value));
+    init_param_t->next = NULL;
+    return init_param_t;
+}
+
+void add_ini_params_node(ini_params **ini_params_t, ini_params *node)
+{
+    node->next = *ini_params_t;
+    *ini_params_t = node;
+}
+
+void visit(ini_params *ini_params_t)
+{
+    printf("ini_param key = %s, value = %s\n", ini_params_t->key, ini_params_t->value);
+}
+
+void print_ini_params_list(ini_params *ini_params_t, void (*visit)(ini_params*))
+{
+    for (; ini_params_t; ini_params_t = ini_params_t->next) {
+        visit(ini_params_t);
+    }
+}
+
+char *ini_params_get(ini_params *ini_params_t, const char *key)
+{
+    size_t keylen = strlen(key);
+    for (; ini_params_t; ini_params_t = ini_params_t->next) {
+        if (!memcmp(ini_params_t->key, key, keylen)) {
+            return ini_params_t->value;
+        }
+    }
+    RETURN_NULL;
+}
+
+void destroy_ini_params(ini_params *ini_params_t)
+{
+    ini_params *q,*p = ini_params_t;
+    while (p) {
+        q = p;
+        p = q->next;
+        free(q);
+    }
+}
+
+ini *create_ini(const char *key, ini_params *ini_params_t)
+{
+    if (!key || !ini_params_t) {
+        RETURN_NULL;
+    }
+    
+    ini *ini_t = MALLOC_INI;
+    if (!ini_t) {
+        RETURN_NULL;
+    }
+    memset(ini_t, 0, sizeof(ini));
+    memset(ini_t->key, 0, sizeof(ini_t->key));
+    
+    memcpy(ini_t->key, key, strlen(key));
+    
+    ini_t->ini_params_t = NULL;
+    add_ini_params_node(&ini_t->ini_params_t, ini_params_t);
+    return ini_t;
+}
+
+void ini_set(ini **ini_ptr, ini *ini_t)
+{
+    ini_t->next = *ini_ptr;
+    *ini_ptr = ini_t;
+}
+
+ini *ini_search(ini *init_t, const char *section)
+{
+    size_t sectionlen = strlen(section);
+    for (; init_t; init_t = init_t->next) {
+        if (!memcmp(init_t->key, section, sectionlen)) {
+            return init_t;
+        }
+    }
+    
+    RETURN_NULL;
+}
+
+char *ini_get(ini *init_t, const char *section, const char *key)
+{
+    
+    size_t sectionlen = strlen(section);
+    for (; init_t; init_t = init_t->next) {
+        if (!memcmp(init_t->key, section, sectionlen)) {
+            return ini_params_get(init_t->ini_params_t, key);
+        }
+    }
+    
+    RETURN_NULL;
+}
+
+void destroy_ini(ini *ini_t)
+{
+    ini *q,*p = ini_t;
+    while (p) {
+        q = p;
+        p = q->next;
+        destroy_ini_params(q->ini_params_t);
+        free(q);
+    }
+}
+
+char *substr(const char *str, size_t start, size_t len)
+{
+    size_t size;
+    if (len == -1) {
+        size = strlen(str) - start;
+    } else {
+        size = len;
+    }
+    char *ret = (char *)malloc(sizeof(char) * size + 1);
+    memcpy(ret, str + start, size);
+    *(ret + size) = '\0';
+    return ret;
 }
 
 ini *parse_ini_file(const char *filename)
 {
-    FILE *fp = fopen(filename, "r");
-    if (fp == NULL) {
-        fprintf(stderr, "read ini file error", errno);
-        return NULL;
+    FILE *fp = fopen(filename, "rb");
+    if (!fp) {
+        fprintf(stderr, "read ini file error!");
+        RETURN_NULL;
     }
-    char line[1024];
     
-    char *d = "=";
-    size_t dlen = strlen(d);
-    ini *node = (ini *)malloc(sizeof(ini));
-    if (!node) {
-        return NULL;
-    }
+    ini *ini_t      = NULL;// = MALLOC_INI;
+    ini *ini_t_ptr  = NULL;
+    
+    char line[1024];
+    char section[32];
+    
+    memset(line, 0, sizeof(line));
+    memset(section, 0, sizeof(section));
+    
+    int     pos = -1;
+    char    *pk = NULL;
+    char    *pv = NULL;
     
     while (fgets(line, 1024, fp)) {
-        if (strlen(line)<=1) {
-            continue;
+        pos = strpos(line, "=");
+        if (-1 != pos && section) {
+            pk = substr(line, 0, pos);
+            pv = substr(line, pos + 1, -1);
+            ini_t_ptr = ini_search(ini_t, section);
+            if (ini_t_ptr && ini_t_ptr->ini_params_t) {
+                add_ini_params_node(&ini_t_ptr->ini_params_t, create_ini_params(pk, pv));
+            } else {
+                ini_set(&ini_t, create_ini(section, create_ini_params(pk, pv)));
+            }
+            free(pk);
+            free(pv);
+        } else {
+            memset(section, 0, sizeof(section));
+            memcpy(section, line, strlen(line));
         }
-        
-        char **rst = str_split(line, strlen(line), d, dlen);
-        if (!rst) {
-            continue;
-        }
-        
-        trim(rst[0]);
-        trim(rst[1]);
-        if (!rst[1]) {
-            free(rst);
-            continue;
-        }
-        
-        ini_set(&node, make_ini(rst[0], rst[1]));
-        free(rst);
     }
-    
-    return node;
+    fclose(fp);
+    return ini_t;
 }
 
-char *ini_get(ini *init, char *key)
-{
-    ini *node = init;
-    while (node) {
-        if (strcmp(node->key, key) == 0) {
-            return node->value;
-        }
-        node = node->next;
-    }
-    
-    return NULL;
-}
+int main(int argc, const char * argv[]) {
+    ini *ini_t = parse_ini_file("/Users/ekikokuiwa/data/code/c/ini/ini/a.ini");
+    printf("value = %s\n", ini_get(ini_t, "[redis]", "host"));
+    printf("value = %s\n", ini_get(ini_t, "[db]", "host"));
 
-void ini_destroy(ini *init)
-{
-    ini *node = init, *tmp = NULL;
-    while (node) {
-        tmp = node;
-        node = node->next;
-        free(tmp);
-    }
-}
-
-char *last_char_is(const char *s, int c)
-{
-    char *sret;
-    if (!s)
-        return NULL;
-    sret = (char *)s+strlen(s)-1;
-    if (sret>=s && *sret == c)
-        return sret;
-    else
-        return NULL;
-}
-
-void chomp(char *s)
-{
-    char *lc = last_char_is(s, '\n');
-
-    if(lc)
-        *lc = 0;
-}
-
-void trim(char *s)
-{
-    int len = strlen(s);
-
-    /* trim trailing whitespace */
-    while ( len > 0 && isspace(s[len-1]))
-        s[--len]='\0';
-
-    /* trim leading whitespace */
-    memmove(s, &s[strspn(s, " \n\r\t\v")], len);
+    destroy_ini(ini_t);
+    return 0;
 }
