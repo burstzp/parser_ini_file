96896f8a3b88ca72837fec5b163d0f4405a37828
yiguopan <yiguopan@126.com>
2015-01-12 08:39:04 +0800

    Update parse_ini_file.h

--- parse_ini_file.h
+++ parse_ini_file.h
@@ -22,4 +22,23 @@ typedef struct _ini_params
     char value[128];
     struct _ini_params *next;
 } ini_params;
+
+int strpos(const char *str, const char *find);
+ini_params *create_ini_params(const char *key, const char *value);
+void add_ini_params_node(ini_params **ini_params_t, ini_params *node);
+void visit(ini_params *ini_params_t);
+void print_ini_params_list(ini_params *ini_params_t, void (*visit)(ini_params*));
+char *ini_params_get(ini_params *ini_params_t, const char *key);
+void destroy_ini_params(ini_params *ini_params_t);
+ini *create_ini(const char *key, ini_params *ini_params_t);
+void ini_set(ini **ini_ptr, ini *ini_t);
+ini *ini_search(ini *init_t, const char *section);
+char *get_section_key(char *key);
+char *ini_get(ini *init_t, const char *section_key, const char *key);
+void destroy_ini(ini *ini_t);
+char *substr(const char *str, size_t start, size_t len);
+ini *parse_ini_file(const char *filename);
+void visit_section(ini *ini_t);
+void print_ini_list(ini *ini_t, void (*visit_section)(ini*));
+
 #endif
