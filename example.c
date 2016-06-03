7c23dee298bdb425b9b08a5b585bb937a23dd70c
burstzp <yiguopan@126.com>
2016-06-03 18:40:38 +0800

    Update example.c
    modify

--- example.c
+++ example.c
@@ -6,11 +6,11 @@ int main() {
     char* buf = load_ini_file("config.ini",&total_bytes);
     char result[1024] = {0};
 
-    read_section_string(buf,total_bytes,"test","a",result);
+    read_section_string(buf,total_bytes,"test","a",result,13);
     char host[1024] = {0};
-    read_section_string(buf,total_bytes,"redis","host",host);
+    read_section_string(buf,total_bytes,"redis","host",host,1024);
 
-    printf("result=%s,%d,%s\r\n",result,read_section_long(buf,total_bytes,"test","b"),host);
+    printf("result=%s,%ld,%s\r\n",result,read_section_long(buf,total_bytes,"test","b"),host);
     ini_file_buffer_free(buf);
 
     return 0;
