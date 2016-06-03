6074ad7eb59583675a086759164d041bea2ee198
burstzp <yiguopan@126.com>
2016-06-03 17:46:36 +0800

    Update ini_file.h
    modify

--- ini_file.h
+++ ini_file.h
@@ -5,7 +5,7 @@ char* load_ini_file(char* file_path,int* total_bytes);
 void ini_file_buffer_free(char* buf);
 int read_section_int(const char* buf, int total_bytes, char* section, char* key);
 long read_section_long(const char* buf, int total_bytes, char* section, char* key);
-void read_section_string(const char* buf, int total_bytes , char* section, char* key, char* result);
+void read_section_string(const char* buf, int total_bytes , char* section, char* key, char* result, int result_len);
 
 #endif
 
